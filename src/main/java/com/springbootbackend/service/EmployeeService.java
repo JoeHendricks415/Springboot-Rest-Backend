ca2dae39334443e0561ca17e5691c92741752806
Joe Hendricks <joehendricks415@gmail.com>
2022-06-23 19:06:14 -0400

    finished services and controllers to interact with db

--- src/main/java/com/springbootbackend/service/EmployeeService.java
+++ src/main/java/com/springbootbackend/service/EmployeeService.java
@@ -2,7 +2,14 @@ package com.springbootbackend.service;
 
 import com.springbootbackend.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(Long Id);
+    Employee updateEmployee(Employee employee, Long id);
+
+    void deleteEmployee(Long id);
 }
